406577adcacc2b3baa58bacbcc8576555c5b15b8
nataliescintron <nataliescintron@gmail.com>
2026-04-30 16:49:43 -0400

    listings

---
+++ app/routes/listings.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, request, jsonify
+from app import db
+from app.models import Listing, Book, User
+
+listings_bp = Blueprint("listings", __name__)
+
+#Create a Listing
+@listings_bp.route("/", methods=["POST"])
+@login_required
+def create_listing():
+    data = request.json
+
+    listing = Listing(
+        id=data["id"],
+        user_id=current_user.id,  
+        book_isbn=data["book_isbn"],
+        condition=data.get("condition"),
+        price=data.get("price"),
+        is_available=True
+    )
+
+    db.session.add(listing)
+    db.session.commit()
+
+    return jsonify({"message": "Listing created"}), 201
+
+#get all listing aka all listing will pop up
+@listings_bp.route("/", methods=["GET"])
+def get_listings():
+    listings = Listing.query.filter_by(is_available=True).all()
+
+    return jsonify([
+        {
+            "id": l.id,
+            "book_title": l.book.title if l.book else None,
+            "author": l.book.author if l.book else None,
+            "price": str(l.price) if l.price else None,
+            "condition": l.condition,
+            "owner": l.owner.f_name if l.owner else None,
+            "images": [img.image_url for img in l.images]
+        }
+        for l in listings
+    ]), 200
+
